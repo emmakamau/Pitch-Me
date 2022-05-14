84bf49080eab1d92b49a4c5e1f379f0c081a764b
EmmaK <emmaculatewkamau@gmail.com>
2022-05-14 16:07:54 +0300

    Set up for deployment

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     pass
 
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
     pass
 
 class DevConfig(Config):
